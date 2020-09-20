ab5d1f1b83015154dff6790266e929ba84d9716b
ruloalv <ruloalv@gmail.com>
2020-09-20 09:19:47 -0300

    print Name + Zipcode (5 digit)

---
+++ lib/event_manager.rb
@@ -0,0 +1,24 @@
+require "csv"
+
+puts "EventManager Initialized!"
+
+=begin
+rescue Exception => e
+	
+end
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+	arrayline = line.split(",")
+	puts arrayline[2]
+end
+=end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row| 
+	name = row[:first_name]
+	zipcode = "00000" + row[:zipcode].to_s
+	
+	zipcode = zipcode.slice(-5,5)
+
+	puts  "#{name} - #{zipcode}"
+end
