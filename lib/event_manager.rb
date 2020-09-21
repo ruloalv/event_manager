8eb2bff7e7952e53750048f6b8db973562959d30
ruloalv <ruloalv@gmail.com>
2020-09-21 07:30:11 -0300

    correction to zipcode format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -16,10 +16,13 @@ end
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row| 
 	name = row[:first_name]
-	zipcode = row[:zipcode].to_s.rjust(5, '00000')
+	zipcode = row[:zipcode].to_s
 
-	#zipcode = "00000" + row[:zipcode].to_s
-	#zipcode = zipcode.slice(-5,5)
-
+	if zipcode.length < 5 
+		zipcode = zipcode.rjust(5, '0')
+	elsif zipcode.length > 5
+		zipcode = zipcode[0..4]
+	end
+	
 	puts  "#{name} - #{zipcode}"
 end
